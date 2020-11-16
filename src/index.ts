ab5cc4071dbca4054cbb5bda65701d48216afcce
pg07codes <pg07codes@gmail.com>
2020-11-16 22:16:45 +0530

    setup local dev environment

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+
+function component() {
+  const element = document.createElement('div');
+
+  console.log('fasdffdfas')
+  element.innerHTML = "fdafad";
+
+  return element;
+}
+
+document.body.appendChild(component());
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.ts',
+    devtool: 'inline-source-map',
+    module: {
+        rules: [{
+            test: /\.ts$/,
+            use: 'ts-loader',
+            exclude: /node_modules/
+        }]
+    },
+    resolve: { // not used currently. no `.js` used.
+        extensions: ['.ts', '.js']
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true
+      }
+};
